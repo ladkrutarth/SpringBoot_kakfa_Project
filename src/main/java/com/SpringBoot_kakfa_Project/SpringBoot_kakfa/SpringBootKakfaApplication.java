fbd26cc23edcf68e788771d5b984c13d19432bf2
krutarthlad <krutarthlad321@gmail.com>
2024-07-16 10:55:35 -0500

    first commit

---
+++ src/main/java/com/SpringBoot_kakfa_Project/SpringBoot_kakfa/SpringBootKakfaApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_kakfa_Project.SpringBoot_kakfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKakfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKakfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot_kakfa_Project/SpringBoot_kakfa/SpringBootKakfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_kakfa_Project.SpringBoot_kakfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKakfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
